71a75c99c1ea84f15c7f7494ea499c965599242f
Alex <perg.alex@gmail.com>
2018-07-14 20:44:27 +0300

    Test commit

---
+++ src/mypackage/PrintAllVariables.java
@@ -0,0 +1,42 @@
+package mypackage;
+
+
+public class PrintAllVariables  {
+
+
+    public void PrintByte (byte var) {
+        System.out.println("Value of Byte is: " + var);
+    }
+
+    public void PrintShort (short var){
+        System.out.println("Value of Short is: " + var);
+    }
+
+    public void PrintInt (int var) {
+        System.out.println("Value of Integer is: " + var);
+    }
+
+    public void PrintLong (long var) {
+        System.out.println("Value of Long is: " + var);
+    }
+
+    public void PrintFloat (float var) {
+        System.out.println("Value of Float is: " + var);
+    }
+
+    public void PrintDouble (double var) {
+        System.out.println("Value of Double is: " + var);
+    }
+
+    public void PrintChar (char var) {
+        System.out.println("Value of Char is: " + var);
+    }
+
+    public void PrintBoolean (boolean var) {
+        System.out.println("Value of Boolean is: " + var);
+    }
+
+    public void PrintString (String str) {
+        System.out.println("And the string is: " + str);
+    }
+}
---
+++ src/mypackage/TestClass.java
@@ -0,0 +1,28 @@
+package mypackage;
+
+public class TestClass {
+    public static void main(String[] args) {
+
+        byte varByte  = 1;
+        short varShort  = -456;
+        int varInt = 45678;
+        long varLong = 1234567891;
+        float varFloat = 5.9f;
+        double varDouble = -1999.745254;
+        char varChar = 'J';
+        boolean varBoolean = true;
+        String str = "Hi all!!!";
+
+        PrintAllVariables printAllVariables = new PrintAllVariables();
+
+        printAllVariables.PrintByte(varByte);
+        printAllVariables.PrintShort(varShort);
+        printAllVariables.PrintInt(varInt);
+        printAllVariables.PrintLong(varLong);
+        printAllVariables.PrintFloat(varFloat);
+        printAllVariables.PrintDouble(varDouble);
+        printAllVariables.PrintChar(varChar);
+        printAllVariables.PrintBoolean(varBoolean);
+        printAllVariables.PrintString(str);
+    }
+}
